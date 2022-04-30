a677e546c0a2dc7865d0ad3062e242eb4d6001c8
loki0925 <shreyashahuja@gmail.com>
2022-04-30 10:12:49 +0530

    Create footer.js

---
+++ comp/footer.js
@@ -0,0 +1,56 @@
+
+const footer=()=>{
+    return `
+    <div id="footer">
+    <p id="foot1">What Our Customer Says</p>
+    <img src="https://niracbd.com/wp-content/uploads/2020/03/quote-100x100.jpg" id="code">
+    <div id="let">
+        <div>Auspicious range of foot wares.
+           Premium branded products. Good
+           service and delivery duration. Super
+           high value of customer.</div>
+        <div>I think one of the best app for the shoes shopping.i m totally happy with it. Thank you so much uboric.</div>
+        <div>Very big range of products and the services are very fast. i ordered footwear and they delivered very quick.</div>
+    </div>
+    <div id="let1">
+       <div>Harpal patel</div>
+       <div>Ishan shah</div>
+       <div>Harshil patel</div>
+   </div>
+   <div id="let3">
+       <div id="let2">
+           <img src="https://community.powerbi.com/oxcrx34285/attachments/oxcrx34285/QuickMeasuresGallery/207/1/5-star-rating.png" alt="">
+           <p>Customer</p>
+       </div>
+       <div id="let2">
+           <img src="https://community.powerbi.com/oxcrx34285/attachments/oxcrx34285/QuickMeasuresGallery/207/1/5-star-rating.png" alt="">
+           <p>Customer</p>
+       </div>
+       <div id="let2">
+           <img src="https://community.powerbi.com/oxcrx34285/attachments/oxcrx34285/QuickMeasuresGallery/207/1/5-star-rating.png" alt="">
+           <p>Customer</p>
+       </div>
+   </div>
+  <div id="btn">Add Your Review</div>
+</div>
+<div id="foot2">
+   <img id="playstore" style="width:200px;" alt="" nitro-lazy-src="https://cdn-fbcdm.nitrocdn.com/lCsZbdoeFoBheKwUKyjvhZqMoGaBchBA/assets/static/optimized/wp-content/uploads/2020/08/eb2afccfcce4f9495c705e85ed0fa175.google-play1.png" class="size-full wp- aligncenter lazyloaded" nitro-lazy-empty="" id="NTM0NDoyMTQ=-1" src="https://cdn-fbcdm.nitrocdn.com/lCsZbdoeFoBheKwUKyjvhZqMoGaBchBA/assets/static/optimized/wp-content/uploads/2020/08/eb2afccfcce4f9495c705e85ed0fa175.google-play1.png">
+   <div id="newnav">
+       <div>About Us</div>
+       <div>Contact</div>
+       <div>Shipping & Returns</div>
+       <div>My Account</div>
+       <div>Privacy Policy</div>
+       <div>Become A Seller</div>
+       <div>Term And Condition</div>
+   </div>
+   <div id="disp">© 2019 – 2021 UBORIC. ALL RIGHTS RESERVED.</div>
+  <div id="maja">
+      <div id="face"> <img src="https://cdn.shopify.com/s/files/1/0972/5586/t/22/assets/social-icon-fb.png?v=11660621197082262703" alt=""> </div>
+      <div id="face"><img src="https://cdn.shopify.com/s/files/1/0972/5586/t/22/assets/social-icon-insta.png?v=15371573375562218970" alt=""></div>
+  </div>
+</div>
+    `
+}
+
+export  default footer 
