9da341da4e2b9389a4ddbdf99a861b8ff1029a2b
loki0925 <shreyashahuja@gmail.com>
2022-05-01 16:14:48 +0530

    Create login.js

---
+++ js/login.js
@@ -0,0 +1,35 @@
+let glad=document.getElementById("glam");
+const text1=[{
+    picc:"https://www.marlec.co.uk/wp-content/uploads/2020/03/tick1.png",
+    lans:"My Account",
+    lans1:"Home",
+   
+   
+}]
+text1.forEach(wel=>{
+    let div=document.createElement("div");
+
+    let div1=document.createElement("div");
+    div1.setAttribute("id","funny")
+   
+   
+
+    let img=document.createElement("img");
+    img.src=wel.picc;
+
+ 
+    let nam=document.createElement("h2");
+    nam.innerText=wel.lans;
+
+    
+    let nam1=document.createElement("h2");
+    nam1.innerText=wel.lans1;
+
+
+  
+
+    div1.append(nam1,img,nam);
+   
+    div.append(div1);
+    glad.append(div);
+})
